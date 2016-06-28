586311b69dd84602e220f846635b70ec1ecec72f
Peter Davis <peter.davis@devworx.com.au>
2016-06-29 00:09:29 +0930

    Update to FlexSlider 2.6.1

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -4,11 +4,10 @@ namespace Tekno.FlexSlider
 {
     public class ResourceManifest : IResourceManifestProvider
     {
-        public void BuildManifests(ResourceManifestBuilder builder)
-        {
-            builder.Add().DefineStyle("FlexSlider").SetUrl("flexslider.min.css", "flexslider.css");
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            builder.Add().DefineStyle("FlexSlider").SetUrl("flexslider.min.css", "flexslider.css").SetVersion("2.6.1");
 
-            builder.Add().DefineScript("FlexSlider").SetUrl("jquery.flexslider.min.js", "jquery.flexslider.js").SetDependencies("jQuery");
+            builder.Add().DefineScript("FlexSlider").SetUrl("jquery.flexslider.min.js", "jquery.flexslider.js").SetDependencies("jQuery").SetVersion("2.6.1");
         }
     }
 }
