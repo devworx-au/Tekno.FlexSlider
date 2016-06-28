96383639ca334bb367a37ca4718701df34732e96
Peter Davis <peter.davis@devworx.com.au>
2016-06-29 00:10:44 +0930

    Added CDN support

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -4,10 +4,18 @@ namespace Tekno.FlexSlider
 {
     public class ResourceManifest : IResourceManifestProvider
     {
-        public void BuildManifests(ResourceManifestBuilder builder) {
-            builder.Add().DefineStyle("FlexSlider").SetUrl("flexslider.min.css", "flexslider.css").SetVersion("2.6.1");
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            builder.Add().DefineStyle("FlexSlider")
+                .SetUrl("flexslider.min.css", "flexslider.css")
+                .SetVersion("2.6.1")
+                .SetCdn("//cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.1/flexslider.min.css", "//cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.1/flexslider.css", true);
 
-            builder.Add().DefineScript("FlexSlider").SetUrl("jquery.flexslider.min.js", "jquery.flexslider.js").SetDependencies("jQuery").SetVersion("2.6.1");
+            builder.Add().DefineScript("FlexSlider")
+                .SetUrl("jquery.flexslider.min.js", "jquery.flexslider.js")
+                .SetDependencies("jQuery")
+                .SetVersion("2.6.1")
+                .SetCdn("//cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.1/jquery.flexslider.min.js", "//cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.1/jquery.flexslider.js", true);
         }
     }
 }
